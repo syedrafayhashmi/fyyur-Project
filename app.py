9752e35c347687febb96f08547d45d8650b3c1db
syedrafayhashmi <rafaysidsid@gmail.com>
2020-04-06 04:37:17 +0500

    project

--- app.py
+++ app.py
@@ -692,6 +692,22 @@ def not_found_error(error):
     return render_template('errors/404.html'), 404
 
 
+@app.errorhandler(500)
+def server_error(error):
+    db.session.rollback()
+    return render_template('errors/500.html'), 500
+
+
+if not app.debug:
+    file_handler = FileHandler('error.log')
+    file_handler.setFormatter(
+        Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    )
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('errors')
 
 #----------------------------------------------------------------------------#
 # Launch.
